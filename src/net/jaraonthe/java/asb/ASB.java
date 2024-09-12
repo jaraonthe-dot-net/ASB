7f9348c919da028aad4fc3e881473767ceaa3613
Jakob Rathbauer <jakob@jaraonthe.net>
2024-09-12 21:23:33 +0200

    Release 0.9-preview
    This is some sort of Alpha version.

--- src/net/jaraonthe/java/asb/ASB.java
+++ src/net/jaraonthe/java/asb/ASB.java
@@ -15,7 +15,7 @@ public class ASB
     /**
      * The current application version.
      */
-    public static final String VERSION = "0.1-dev";
+    public static final String VERSION = "0.9-preview";
     
     public static final String HELP_TEXT =
         "Usage:\n"
